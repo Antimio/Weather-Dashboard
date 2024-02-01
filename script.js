b35c4268577e2f8db2537d5830519270f76ebf33
Antimio <antimiocasimiro@gmail.com>
2024-02-01 22:06:34 +0000

    Added a fetch that retrieves the latitude and longitude and that, then, uses those items to call another fetch that produces the weather data required. So, a nested fetch.

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ var APIKey = "5eac79e569460d81d76fc4d3f59f89fc";
 var quordinatesAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q="
 
 var cityHistory = [];
+
+function weatherData(city) {
+
+    var queryURLCoordinates = quordinatesAPIURL + city + "&limit=5&appid=" + APIKey;
+
+    fetch(queryURLCoordinates)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+                console.log(quordinatesAPIURL);
+                console.log(data);
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+
+                var weatherForecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=";
+
+                var queryURL = weatherForecastURL + lat + "&lon=" + lon + "&units=metric&appid=" + APIKey;
+
+                fetch(queryURL)
+                    .then(function(response) {
+                        return response.json();
+                    }).then(function(moreData) {
+                        console.log(queryURL);
+                        console.log(moreData);
+
+                        todayWeather(moreData);
+                        fiveDayForecast(moreData);
+                        addToHistory(city);
+
+
+
+                    });
+
+
+            }
+
+        )
+}
