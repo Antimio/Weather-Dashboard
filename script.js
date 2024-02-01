3f95a6b196ed5af6a6511d472c4b130cc2e38183
Antimio <antimiocasimiro@gmail.com>
2024-02-01 23:08:40 +0000

    corrected something that may be causing issues with the deployment of the page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var APIKey = "5eac79e569460d81d76fc4d3f59f89fc";
 
-var quordinatesAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q="
+var quordinatesAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
 var cityHistory = [];
 
