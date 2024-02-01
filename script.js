44d642ccce317340056c1c08cf7f73840b3cf13e
Antimio <antimiocasimiro@gmail.com>
2024-02-01 22:18:40 +0000

    added the code that will produce the five-day forecast. The for loop move in eigths because there are 40 items on the list and every 8 items represent 24 hours elapsed

--- script.js
+++ script.js
@@ -68,3 +68,39 @@ function todayWeather(moreData) {
 
     todayWeatherContainer.append(cityName.append(date).append(icon), temp, wind, humidity);
 }
+
+function fiveDayForecast(moreData) {
+    var forecasrEl = $("#forecast");
+
+    forecasrEl.empty();
+
+    var forecastTitle = $("<h4>").text("5-Day Forecast:").addClass("mt-1 h4 form-label");
+
+    var forecastElementRow = $("<div>").addClass("row align-items-center d-flex justify-content-center");
+
+    forecasrEl.append(forecastTitle, forecastElementRow);
+
+    for (let index = 0; index < moreData.list.length; index += 8) {
+
+        var forecastElement = moreData.list[index];
+
+        var forecastColumn = $("<div>").addClass("col-md-2 bg-info text-white p-1 border");
+
+        var date = $("<h5>").text(" (" + dayjs(forecastElement.dt_txt).format("DD/MM/YYYY") + ")");
+
+        var icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastElement.weather[0].icon + ".png");
+
+        var temp = $("<p>").text("Temp: " + forecastElement.main.temp + " °C");
+
+        var wind = $("<p>").text("Wind : " + forecastElement.wind.speed + " KPH");
+
+        var humidity = $("<p>").text("Humidity : " + forecastElement.main.humidity + " %");
+
+        forecastColumn.append(date, icon, temp, wind, humidity);
+
+        forecastElementRow.append(forecastColumn);
+
+    }
+
+
+}
